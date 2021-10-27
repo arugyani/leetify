2e2fd1ddbe22e189bb4d8381d88c01f358791a20
Aru Gyani <gyaniaru@gmail.com>
2021-10-26 22:00:29 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const axios = require('axios');
+const fs = require('fs');
+
+const app = express();
+
+app.use(express.json());
+
+let leetcode = 'https://leetcode.com/graphql';
+
+let listQuery = fs.readFileSync("./queries/lists.gql").toString();
+let questionQuery = fs.readFileSync("./queries/question.gql").toString();
+
+// GET all questions
+app.get('/list', (req, res) => {
+    axios({
+        url: leetcode,
+        method: 'POST',
+        data: {
+            query: listQuery,
+            variables: {
+                categorySlug: "",
+                filters: {},
+                limit: null,
+                skip: 0
+            }
+        }
+    }).then((result) => {
+        res.json(result.data);
+    });
+});
+
+// GET list by difficulty
+app.get('/list/:difficulty', (req, res) => {
+    let { difficulty } = req.params;
+    let filters;
+
+    difficulty = difficulty.toUpperCase();
+
+    if (difficulty.toUpperCase() != "EASY" && difficulty != "MEDIUM" && difficulty != "HARD") filters = {};
+    else filters = {"difficulty": difficulty.toUpperCase() }
+
+    axios({
+        url: leetcode,
+        method: 'POST',
+        data: {
+            query: listQuery,
+            variables: {
+                categorySlug: "",
+                filters: filters,
+                limit: null,
+                skip: 0
+            }
+        }
+    }).then((result) => {
+        res.json(result.data);
+    });
+});
+
+// GET question by name
+app.get('/question/:name', (req, res) => {
+    let { name } = req.params
+
+    axios({
+        url: 'https://leetcode.com/graphql',
+        method: 'POST',
+        data: {
+            query: questionQuery,
+            variables: {
+                titleSlug: name
+            }
+        }
+    }).then((result) => {
+        if (result.data['data']['question'] == null) res.json({"error": "question does not exist"});
+        else res.json(result.data);
+    });
+});
+
+app.listen(8080, () => console.log('API listening on port 8080'));
